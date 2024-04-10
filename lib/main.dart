6d280051c322b8f59f44899de59272cf971d4c0a
MidhileshRaj <midhileshraj01@gmail.com>
2024-04-10 22:18:33 +0530

    web_app started

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(useMaterial3: true),
+      // home: ,
+    );
+  }
+}
